91130d694295669e8132b3d6abe34931757f504f
Niranjanaa <nragupathy3@gatech.edu>
2012-10-28 12:53:40 -0400

    Session set

--- WTM/src/com/wtm/todo/LoginServlet.java
+++ WTM/src/com/wtm/todo/LoginServlet.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -22,6 +23,7 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 public class LoginServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		HttpSession session = req.getSession(true);
 		String name = req.getParameter("username");
 		String pass = req.getParameter("password");
 		
@@ -35,6 +37,7 @@ public class LoginServlet extends HttpServlet {
 			for ( Entity thisUser : usersExist ){
 				String returnedPassword = (String) thisUser.getProperty("Password");
 				if( pass.equals(returnedPassword) ){
+					session.setAttribute("User", thisUser.getProperty("Username"));
 					resp.sendRedirect("home.jsp");
 				}
 			}
